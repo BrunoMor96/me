1b413bb6b1be38991405df0f9c481ae29b36215c
Bruno <brunoisil9696@gmail.com>
2020-08-11 22:25:09 -0500

    Updates

---
+++ precache-manifest.dd674ec5fe68c2dc4809771e4d59e48c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "aec8b8528adeaa79c0c9",
+    "url": "/me/static/js/main.aec8b852.chunk.js"
+  },
+  {
+    "revision": "1e60e41b319ce9753b2c",
+    "url": "/me/static/js/1.1e60e41b.chunk.js"
+  },
+  {
+    "revision": "f4ae667e0f9864d95541",
+    "url": "/me/static/js/runtime~main.f4ae667e.js"
+  },
+  {
+    "revision": "6e6cf79f4a87015d6f47802746a82d4d",
+    "url": "/me/index.html"
+  }
+];
